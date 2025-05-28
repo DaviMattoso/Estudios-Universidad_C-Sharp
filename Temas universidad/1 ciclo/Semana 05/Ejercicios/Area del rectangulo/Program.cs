e9bb8189fdba8242d12351948ae56c28d82fd145
Davi Mattoso Cavalcanti <davimattoso35@gmail.com>
2025-05-27 21:30:34 -0300

    Add Semana 05

---
+++ Temas universidad/1 ciclo/Semana 05/Ejercicios/Area del rectangulo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int largo, ancho, area; // Definir variables como entero
+
+        Console.WriteLine("Ingrese el largo del rectangulo"); // Ingresar la variable largo
+        largo = int.Parse(Console.ReadLine()); // Leer la variablre largo
+
+        Console.WriteLine("Ingrese el ancho del rectangulo"); //Ingresar la variable ancho
+        ancho = int.Parse(Console.ReadLine()); // Leer la variable ancho
+
+        area = largo * ancho; // Operación para calcular área
+
+        Console.WriteLine("El area del rectangulo es:" + area); // Salida del resultado
+    }
+}
---
+++ Temas universidad/1 ciclo/Semana 05/Pseudo código/AreaRectangulo.psc
@@ -0,0 +1,11 @@
+Algoritmo AreaRectangulo
+	Definir Largo Como Entero
+	Definir Ancho Como Entero
+	Definir Area Como Entero
+	Escribir "Ingrese el largo del rectangulo"
+	Leer Largo
+	Escribir "Ingrese el ancho del rectangulo"
+	Leer Ancho
+	Area <- Largo * Ancho
+	Escribir "La area del rectangulo es:", Area
+FinAlgoritmo
